9e8d44bd4a96439762c1f71b5980a77971d61908
RAJARSHI DAS <drajarshi@in.ibm.com>
2020-02-05 12:54:44 +0530

    Add ssh key filename as a input configuration.

--- perf/run_ng_vpnaas.py
+++ perf/run_ng_vpnaas.py
@@ -13,17 +13,18 @@ import json
 
 # ls -l for test purposes.
 class list_dir:
-	def __init__(self,ip,args=[" -l > "," 2>&1 &"]):
+	def __init__(self,ip,key_filename,args=[" -l > "," 2>&1 &"]):
 		print("list_dir: __init__");
 		self.command = "/bin/ls";
 		self.ip = ip;
 		self.args = args;
+		self.key_filename = key_filename;
 	
 	def exec_listdir(self):
 		try:
 			conn = Connection(self.ip,user="root",
 				connect_kwargs={
-					"key_filename":"/home/rajarshi/.ssh/id_rsa_vpnaas_ng_prod",
+					"key_filename":self.key_filename,
 				},
 			)
 			print("exec_listdir: conn: ",conn);
@@ -49,16 +50,17 @@ class list_dir:
 		
 
 class mpstat:
-	def __init__(self,ip,args=["-P ALL 2 > ", " 2>&1 &"]):
+	def __init__(self,ip,key_filename,args=["-P ALL 2 > ", " 2>&1 &"]):
 		self.command = "/usr/bin/mpstat";
 		self.args = args;
 		self.ip = ip;
+		self.key_filename = key_filename;
 
 	def exec_mpstat(self):
 		try:
 			conn = Connection(self.ip,user="root",
 				connect_kwargs={
-					"key_filename":"/home/rajarshi/.ssh/id_rsa_vpnaas_ng_prod",
+					"key_filename":self.key_filename,
 				},
 			)
 		except ValueError as e:
@@ -83,16 +85,17 @@ class mpstat:
 
 		
 class vmstat:
-	def __init__(self,ip,args=[" -t 2 > "," 2>&1 &"]):
+	def __init__(self,ip,key_filename,args=[" -t 2 > "," 2>&1 &"]):
 		self.command = "/usr/bin/vmstat";
 		self.args = args;
 		self.ip = ip;
+		self.key_filename = key_filename;
 
 	def exec_vmstat(self):
 		try:
 			conn = Connection(self.ip,user="root",
 				connect_kwargs={
-					"key_filename":"/home/rajarshi/.ssh/id_rsa_vpnaas_ng_prod",
+					"key_filename":self.key_filename,
 				},
 			)
 		except ValueError as e:
@@ -116,11 +119,11 @@ class vmstat:
 		t_vmstat.start();
 
 class monitors():
-	def __init__(self,ip,mpstat_args,vmstat_args):
+	def __init__(self,ip,mpstat_args,vmstat_args,key_filename):
 		print('in monitors init. ip: ',ip);
-		self.mpstat = mpstat(ip);
-		self.vmstat = vmstat(ip);
-		self.listdir = list_dir(ip);
+		self.mpstat = mpstat(ip,key_filename);
+		self.vmstat = vmstat(ip,key_filename);
+		self.listdir = list_dir(ip,key_filename);
 
 	def start(self):
 		print('in monitors start.');
@@ -129,16 +132,17 @@ class monitors():
 		#self.listdir.start();
 
 class iperf3_server:
-	def __init__(self,ip,args=[" -s > "," 2>&1 &"]):
+	def __init__(self,ip,key_filename,args=[" -s > "," 2>&1 &"]):
 		self.command = "/usr/bin/iperf3";
 		self.ip = ip;
 		self.args = args;
+		self.key_filename = key_filename;
 
 	def start(self):
 		try:
 			conn = Connection(self.ip,user="root",
 				connect_kwargs={
-					"key_filename":"/home/rajarshi/.ssh/id_rsa_vpnaas_ng_prod",
+					"key_filename":self.key_filename,
 				},
 			)
 		except ValueError as e:
@@ -157,18 +161,19 @@ class iperf3_server:
 			exit(-1);
 	
 class iperf3_client:
-	def __init__(self,client_ip,server_ip,config="input.config",args=[" > "," 2>&1 &"]):
+	def __init__(self,client_ip,server_ip,key_filename,config="input.config",args=[" > "," 2>&1 &"]):
 		self.command = "/root/iperf3_test.py";
 		self.config_file = "/root/" + config;
 		self.args = args;
 		self.ip = client_ip;
 		self.server_ip = server_ip;
+		self.key_filename = key_filename;
 
 	def start(self):
 		try:
 			conn = Connection(self.ip,user="root",
 				connect_kwargs={
-					"key_filename":"/home/rajarshi/.ssh/id_rsa_vpnaas_ng_prod",
+					"key_filename":self.key_filename,
 				},
 			)
 		except ValueError as e:
@@ -189,9 +194,9 @@ class iperf3_client:
 		
 	
 class server():
-	def __init__(self,server_ip,mpstat_args=None,vmstat_args=None):
-		self.monitors = monitors(server_ip,mpstat_args,vmstat_args);
-		self.iperf3_server = iperf3_server(server_ip);
+	def __init__(self,server_ip,key_filename,mpstat_args=None,vmstat_args=None):
+		self.monitors = monitors(server_ip,mpstat_args,vmstat_args,key_filename);
+		self.iperf3_server = iperf3_server(server_ip,key_filename);
 
 	def start_run(self):
 		print("server: in start_run");
@@ -199,9 +204,9 @@ class server():
 		self.iperf3_server.start();
 		
 class client:
-	def __init__(self,client_ip,server_ip,client_config,mpstat_args=None,vmstat_args=None):
-		self.monitors = monitors(client_ip,mpstat_args,vmstat_args);
-		self.iperf3_client = iperf3_client(client_ip,server_ip,client_config);
+	def __init__(self,client_ip,server_ip,client_config,key_filename,mpstat_args=None,vmstat_args=None):
+		self.monitors = monitors(client_ip,mpstat_args,vmstat_args,key_filename);
+		self.iperf3_client = iperf3_client(client_ip,server_ip,key_filename,client_config);
 
 	def start_run(self):
 		print("client: in start_run");
@@ -209,9 +214,9 @@ class client:
 		self.iperf3_client.start();
 
 class run_combination():
-	def __init__(self,client_ip,server_ip,client_config):
-		self.server = server(server_ip);
-		self.client = client(client_ip,server_ip,client_config);
+	def __init__(self,client_ip,server_ip,client_config,key_filename):
+		self.server = server(server_ip,key_filename);
+		self.client = client(client_ip,server_ip,client_config,key_filename);
 
 	def start_server(self):
 		self.server.start_run();
@@ -224,8 +229,8 @@ class run_combination():
 # All routines to be started within a thread context
 #def start_client_server_pair(client_server_addresses):
 def start_client_server_pair(client_server_data):
-	client_ip,server_ip,client_config = client_server_data;
-	rc = run_combination(client_ip,server_ip,client_config);
+	client_ip,server_ip,client_config,key_filename = client_server_data;
+	rc = run_combination(client_ip,server_ip,client_config,key_filename);
 	rc.start_server();
 	rc.start_client();
 
@@ -239,7 +244,8 @@ def get_config(configfile="cli_serv.config"):
 	print('data after json load: ',data);
 	
 	for i in np.arange(0,len(data)):
-		cli_serv_pairs.append((data[i]["client"],data[i]["server"],data[i]["client_config"]));
+		cli_serv_pairs.append((data[i]["client"],data[i]["server"],data[i]["client_config"],\
+				data[i]["key_filename"]));
 
 	return cli_serv_pairs;
 
@@ -254,7 +260,15 @@ if __name__ == "__main__":
 		for i in np.arange(0,int(num_combinations)):
 			print("Enter client IP and server IP separated by a space, one pair of ips per line");
 			client_ip,server_ip = input().split(' ');
-			cli_serv_pairs.append((client_ip,server_ip));
+			print("Enter the name of the configuration file in the root folder of the client");
+			client_config = input();
+			print("Enter the name of the file (including path) locally present, that contains \
+				 the ssh key to connect to the clients and servers");
+			key_filename = input();
+			if (len(key_filename)==0):
+				print("ssh key filename must be specified to proceed further. Exiting.");
+				exit(-1);
+			cli_serv_pairs.append((client_ip,server_ip,client_config,key_filename));
 
 	for i in np.arange(0,len(cli_serv_pairs)):
 		run_thread = Thread(target=start_client_server_pair,args=(cli_serv_pairs[i],));
